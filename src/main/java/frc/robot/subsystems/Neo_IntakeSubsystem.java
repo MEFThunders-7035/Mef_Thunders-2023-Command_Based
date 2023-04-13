c723f107c42460200633152c177f627fde832def
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-04-13 06:07:52 +0300

    delete unnessicary imports

--- src/main/java/frc/robot/subsystems/Neo_IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Neo_IntakeSubsystem.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
