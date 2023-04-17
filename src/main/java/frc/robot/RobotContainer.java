1fa631b521d9dc35f7d167da4effe95291477cc7
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-04-17 13:20:46 +0300

    Organize imports

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,23 +14,26 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
+
 import frc.robot.Constants.AutonomousConstants;
 import frc.robot.Constants.HorizontalElevatorConstants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.IoConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.VerticalElevatorConstants;
+
 import frc.robot.commands.ArcadeDriveCmd;
 import frc.robot.commands.HoldIntakeCmd;
 import frc.robot.commands.HorizontalElevatorJoystickCmd;
 import frc.robot.commands.IntakeNeoJoystickCmd;
 import frc.robot.commands.IntakeRedlineJoystickCmd;
 import frc.robot.commands.SetCompressorCmd;
-import frc.robot.commands.VerticalElevatorJoystickCmd;
-import frc.robot.commands.VisionTargettingCmd;
+import frc.robot.commands.SetSelenoidsCmd;
 import frc.robot.commands.TimedDriveCmd;
 import frc.robot.commands.TimedIntakeRedlineCmd;
-import frc.robot.commands.SetSelenoidsCmd;
+import frc.robot.commands.VerticalElevatorJoystickCmd;
+import frc.robot.commands.VisionTargettingCmd;
+
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.HorizontalElevatorSubsytem;
 import frc.robot.subsystems.Neo_IntakeSubsystem;
