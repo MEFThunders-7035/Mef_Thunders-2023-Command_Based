bc10dc25f40721467bd0e684c560f8037622ffc7
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-04-17 13:53:19 +0300

    Put Limit Switch info to Dashboard

--- src/main/java/frc/robot/subsystems/VerticalElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/VerticalElevatorSubsystem.java
@@ -5,6 +5,7 @@ import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.HorizontalElevatorConstants;
 
@@ -13,13 +14,28 @@ public class VerticalElevatorSubsystem extends SubsystemBase{
     public static final Spark Elevator = new Spark(HorizontalElevatorConstants.kHorizontalElevatorPort);
     private final DigitalInput toplimitSwitch = new DigitalInput(0);
     private final DigitalInput bottomlimitSwitch = new DigitalInput(1);
+    private boolean topLimitSwitch_temp;
+    private boolean bottomLimitSwitch_temp;
 
 
     public VerticalElevatorSubsystem() {
+        topLimitSwitch_temp = getTopLimitSwitch();
+        bottomLimitSwitch_temp = getBottomLimitSwitch();
+        SmartDashboard.putBoolean("Top Limit Switch", getTopLimitSwitch());
+        SmartDashboard.putBoolean("Bottom Limit Switch", getBottomLimitSwitch());
         Elevator.setInverted(true);
     }
     
-    
+    @Override
+    public void periodic() {
+        if (topLimitSwitch_temp != getTopLimitSwitch()) {
+            topLimitSwitch_temp = getTopLimitSwitch();
+            SmartDashboard.putBoolean("Top Limit Switch", getTopLimitSwitch());
+        }
+        if (bottomLimitSwitch_temp != getBottomLimitSwitch()) {
+            bottomLimitSwitch_temp = getBottomLimitSwitch();
+            SmartDashboard.putBoolean("Bottom Limit Switch", getBottomLimitSwitch());
+        }
 
         if (!Elevator.isAlive()) {
             DriverStation.reportError("Elevator motor is not alive", false);
