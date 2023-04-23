0d468969c94e27e1a3306e335a0c3cb219e57359
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-04-24 00:42:35 +0300

    Small fixes

--- src/main/java/frc/robot/commands/TimedDriveCmd.java
+++ src/main/java/frc/robot/commands/TimedDriveCmd.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
@@ -14,6 +15,8 @@ public class TimedDriveCmd extends CommandBase{
     * @param driveSubsystem the drive subsystem
     * @param speed the speed to drive at (between -1 and 1)
     * @param time the time to drive for (in seconds)
+    * @throws InterruptedException if the thread is interrupted
+    * @throws IllegalArgumentException if the speed is not between -1 and 1
     * @return the command
     */
     public TimedDriveCmd(DriveSubsystem driveSubsystem, double speed, double time) {
@@ -28,13 +31,19 @@ public class TimedDriveCmd extends CommandBase{
     }
     @Override
     public void execute() {
+        if (Math.abs(speed) > 1) throw new IllegalArgumentException("Speed must be between -1 and 1");
         new Thread(() -> {
             try {
+                System.out.println("TimedDriveCommand started");
                 driveSubsystem.drive(speed, 0);
+                System.out.println("TimedDriveCommand waiting for " + time + " seconds");
                 Thread.sleep((long) (time * 1000));
                 driveSubsystem.drive(0, 0);
             } catch (InterruptedException e) {
+                driveSubsystem.setMotors(0, 0);
+                DriverStation.reportError("Auto Command interrupted", e.getStackTrace());
                 e.printStackTrace();
+                throw new RuntimeException(e);
             }
             finlished = true;
         }).start();
@@ -43,11 +52,11 @@ public class TimedDriveCmd extends CommandBase{
     public void end(boolean interrupted) {
         driveSubsystem.drive(0, 0);
         if (interrupted) {
-            System.out.println("DriveBackwardsCmd was interrupted");
+            System.out.println("TimedDriveCommand was interrupted");
             finlished = false;
         }
         else {
-            System.out.println("DriveBackwardsCmd finished");
+            System.out.println("TimedDriveCommand finished");
             finlished = true;
         }
     }
