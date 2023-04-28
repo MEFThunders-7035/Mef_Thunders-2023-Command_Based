58e0584541798201d3da25035bf9dedb4991c1b3
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-04-29 01:07:50 +0300

    docs on navX

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -54,13 +54,33 @@ public class DriveSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
+  /**
+   * Sets the speed of the motors
+   * @param LeftMotorSpeed double between -1 and 1
+   * @param RightMotorSpeed double between -1 and 1
+   */
   public void setMotors(double LeftMotorSpeed, double RightMotorSpeed) {
     leftMotorsGroup.set(LeftMotorSpeed);
     rightMotorsGroup.set(RightMotorSpeed);
   }
 
-  public void drive(double speed, double rotation) {
-    driveTrain.arcadeDrive(-speed, -rotation);
+  /**
+   * Sets the speed of the motors
+   * @param xSpeed The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+   * @param zRotation The robot's rotation rate around the Z axis [-1.0..1.0]. Counterclockwise is positive.
+   */
+  public void drive(double xSpeed, double zRotation) {
+    driveTrain.arcadeDrive(-xSpeed, -zRotation);
+  }
+
+  /**
+   * Sets the speed of the motors
+   * @param xSpeed The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+   * @param zRotation The robot's rotation rate around the Z axis [-1.0..1.0]. Counterclockwise is positive.
+   * @param squaredInputs If set, decreases the input sensitivity at low speeds.
+   */
+  public void drive(double xSpeed, double zRotation, boolean squaredInputs) {
+    driveTrain.arcadeDrive(-xSpeed, -zRotation, squaredInputs);
   }
 
   public void stop() {
@@ -71,31 +91,58 @@ public class DriveSubsystem extends SubsystemBase {
     driveTrain.setSafetyEnabled(enabled);
   }
 
+  /**
+   * Resets the encoders
+   */
   public void resetEncoders() {
     leftEncoder.reset();
     rightEncoder.reset();
   }
 
+  /**
+   * Gets the distance the left encoder has traveled
+   * @return double distance in meters
+   */
   public double getLeftEncoderDistance() {
     return leftEncoder.getDistance();
   }
 
+  /**
+   * Gets the distance the right encoder has traveled
+   * @return double distance in meters
+   */
   public double getRightEncoderDistance() {
     return rightEncoder.getDistance();
   }
 
+  /**
+   * Gets the rate of the left encoder
+   * @return double rate in meters per second
+   */
   public double getLeftEncoderRate() {
     return leftEncoder.getRate();
   }
 
+  /**
+   * Gets the rate of the right encoder
+   * @return double rate in meters per second
+   */
   public double getRightEncoderRate() {
     return rightEncoder.getRate();
   }
   
+  /**
+   * Finds if the NavX is connected
+   * @return boolean true if connected
+   */
   public boolean getGyroIsConnected() {
     return navX.isConnected();
   }
 
+  /**
+   * Calibrates the navX
+   * @throws RuntimeException if the navX is not connected
+   */
   public void calibrateGyro() {
     if (!getGyroIsConnected()) {
       throw new RuntimeException("NavX is not connected");
@@ -103,6 +150,10 @@ public class DriveSubsystem extends SubsystemBase {
     navX.calibrate();
   }
 
+  /**
+   * Resets the navX
+   * @throws RuntimeException if the navX is not connected
+   */
   public void resetGyro() {
     if (!getGyroIsConnected()) {
       throw new RuntimeException("NavX is not connected");
@@ -110,6 +161,11 @@ public class DriveSubsystem extends SubsystemBase {
     navX.reset();
   }
 
+  /**
+   * Gets the rotation of the navX in {@link Rotation2d}
+   * @return Rotation2d
+   * @throws RuntimeException if the navX is not connected
+   */
   public Rotation2d getGyroRotation2d() {
     if (!getGyroIsConnected()) {
       throw new RuntimeException("NavX is not connected");
@@ -117,6 +173,14 @@ public class DriveSubsystem extends SubsystemBase {
     return navX.getRotation2d();
   }
 
+  /**
+   * Gets the rotation of the navX in degrees
+   * NOTE: The angle is continuous, meaning it's range is beyond 360 degrees. 
+   * This ensures that algorithms that wouldn't want to see a discontinuity in the gyro output 
+   * as it sweeps past 0 on the second time around.
+   * if you want it to be fixed use {@link DriveSubsystem#getGyroAngleFixed}
+   * @return the total accumilated yaw angle (Z axis) double rotation in degrees 
+   */
   public double getGyroAngle() {
     if (!getGyroIsConnected()) {
       throw new RuntimeException("NavX is not connected");
@@ -124,6 +188,22 @@ public class DriveSubsystem extends SubsystemBase {
     return navX.getAngle();
   }
 
+  /**
+   * Gets the rotation of the navX in degrees but does not go past 360 degrees.
+   * if you want it to be continuous use {@link DriveSubsystem#getGyroAngle}
+   * @return the yaw angle (Z axis) double rotation in degrees
+   */
+  public double getGyroAngleFixed() {
+    if (!getGyroIsConnected()) {
+      throw new RuntimeException("NavX is not connected");
+    }
+    return navX.getAngle() % 360;
+  }
+
+  /**
+   * Gets the rate of rotation of the navX in degrees per second
+   * @return double rotation in degrees per second
+   */
   public double getGyroRate() {
     if (!getGyroIsConnected()) {
       throw new RuntimeException("NavX is not connected");
@@ -131,6 +211,10 @@ public class DriveSubsystem extends SubsystemBase {
     return navX.getRate();
   }
 
+  /**
+   * gets if Gyro Is Calibrating
+   * @return boolean true if calibrating
+   */
   public boolean getGyroIsCalibrating() {
     if (!getGyroIsConnected()) {
       throw new RuntimeException("NavX is not connected");
