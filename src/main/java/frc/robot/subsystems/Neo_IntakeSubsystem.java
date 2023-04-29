5d707a5dd0edb9455459bf75207742ebc58a5302
kytpbs <95276965+kytpbs@users.noreply.github.com>
2023-04-29 21:43:35 +0300

    Neo Unit Test

--- src/main/java/frc/robot/subsystems/Neo_IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Neo_IntakeSubsystem.java
@@ -1,17 +1,25 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.REVLibError;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
-public class Neo_IntakeSubsystem extends SubsystemBase{
+public class Neo_IntakeSubsystem extends SubsystemBase implements AutoCloseable{
 
     public static final CANSparkMax Neo = new CANSparkMax(IntakeConstants.kCanNeoPort, CANSparkMax.MotorType.kBrushless);
+    
     public Neo_IntakeSubsystem() {
         
     }
+
+    @Override
+    public void close() throws Exception {
+        return;
+    }
+
     @Override
     public void periodic() {
         SmartDashboard.putNumber("Neo Encoder", Neo.getEncoder().getPosition());
@@ -20,6 +28,65 @@ public class Neo_IntakeSubsystem extends SubsystemBase{
     }
     public void setNeo(double speed) {
         Neo.set(speed);
+    }
+    
+    public double getNeoLastSet() {
+        return Neo.get();
+    }
+    public REVLibError getNeoError() {
+        return Neo.getLastError();
+    }
+    public double getNeoRampRate() {
+        return Neo.getOpenLoopRampRate();
+    }
+
+    public boolean getNeoBrakeMode() {
+        if (Neo.getIdleMode().equals(CANSparkMax.IdleMode.kBrake)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * get the fault bits of the Neo
+     * @return the fault bits of the Neo as a short
+     */
+    public short getNeoFaults() {
+        return Neo.getFaults();
+    }
+
+    /**
+     * Set the ramp rate of the Neo
+     * @param rate the ramp rate in seconds from 0 to full speed
+     */
+    public void setNeoRampRate(double rate) {
+        Neo.setOpenLoopRampRate(rate);
+    }
+
+    public void setNeoCurrentLimit(int limit) {
+        Neo.setSmartCurrentLimit(limit);
+    }
+
+    
+    /**
+     * Stop the Neo
+     */
+    public void stopNeo() {
+        Neo.stopMotor();
+    }
+
+    /**
+     * Set the idle mode of the Neo
+     * @param brake true = brake, false = coast
+     */
+    public void setNeoBrake(boolean brake) {
+        Neo.setIdleMode(brake ? CANSparkMax.IdleMode.kBrake : CANSparkMax.IdleMode.kCoast);
+    }
+    /**
+     * @return the motor temperature of the Neo in Celsius
+     */
+    public double getNeoTempreture() {
+        return Neo.getMotorTemperature();
     }
     public Double getNeoPos() {
         return Neo.getEncoder().getPosition();
---
+++ src/test/java/NeoTest.java
@@ -0,0 +1,58 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.hal.HAL;
+import frc.robot.subsystems.Neo_IntakeSubsystem;
+
+public class NeoTest {
+    Neo_IntakeSubsystem neoSubsystem;
+
+    @BeforeEach
+    void setup() {
+        HAL.initialize(800, 0);
+        neoSubsystem = new Neo_IntakeSubsystem();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        neoSubsystem.stopNeo();
+        neoSubsystem.close();
+    }
+
+    @Test
+    void setNeoTest() throws Exception {
+        neoSubsystem.setNeo(1.0);
+        assertEquals(1.0, neoSubsystem.getNeoLastSet());
+        neoSubsystem.setNeo(-1.0);
+        assertEquals(-1.0, neoSubsystem.getNeoLastSet());
+    }
+
+    @Test
+    void stopNeoTest() throws Exception {
+        neoSubsystem.setNeo(1.0);
+        neoSubsystem.stopNeo();
+        assertEquals(0.0, neoSubsystem.getNeoLastSet());
+    }
+
+    @Test
+    void NeoSetBrakeModeTest() throws Exception {
+        neoSubsystem.setNeoBrake(false);
+        assertEquals(false, neoSubsystem.getNeoBrakeMode());
+        neoSubsystem.setNeoBrake(true);
+        assertEquals(true, neoSubsystem.getNeoBrakeMode());
+    }
+
+    @Test
+    void setNeoCurrentLimitTest() {
+        neoSubsystem.setNeoCurrentLimit(40);
+    }
+
+    @Test
+    void setNeoRampRateTest() {
+        neoSubsystem.setNeoRampRate(0.5);
+        assertEquals(0.5, neoSubsystem.getNeoRampRate());
+    }
+}
