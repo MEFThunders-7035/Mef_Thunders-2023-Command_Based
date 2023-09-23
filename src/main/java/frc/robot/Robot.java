a2f513ab5e26dc429e42d287f5509ca479a5b452
Kaya <95276965+kytpbs@users.noreply.github.com>
2023-09-23 12:49:19 +0300

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,8 +23,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    addPeriodic(m_robotContainer::fast_periodic,
-     0.005);
+    addPeriodic(m_robotContainer::fast_periodic, 0.005);
   }
 
   
